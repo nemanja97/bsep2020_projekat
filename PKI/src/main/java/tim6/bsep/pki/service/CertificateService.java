437468f7c24e4cf79a2301a6d43008f43e73dea5
Nemanja Janković <nemanja997@gmail.com>
2020-04-06 22:52:17 +0200

    Added untested revoke

--- PKI/src/main/java/tim6/bsep/pki/service/CertificateService.java
+++ PKI/src/main/java/tim6/bsep/pki/service/CertificateService.java
@@ -1,11 +1,33 @@
 package tim6.bsep.pki.service;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.CRLReason;
+import org.bouncycastle.asn1.x509.Extensions;
+import org.bouncycastle.cert.X509CRLHolder;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v2CRLBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import tim6.bsep.pki.generator.KeyPairGenerator;
+import tim6.bsep.pki.model.IssuerData;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.KeyPair;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Date;
 
 public class CertificateService {
+
+    @Autowired
+    private KeyStoreService keyStoreService;
 
     public X509Certificate createSelfSignedCertificate() {
         KeyPair keyPair = KeyPairGenerator.generateKeyPair();
@@ -24,7 +46,46 @@ public class CertificateService {
         return false;
     }
 
-    public boolean revokeCertificate() {
+    public boolean revokeCertificate(String id, CRLReason reason) throws CertificateException, OperatorCreationException {
+        X509Certificate certificate = (X509Certificate) keyStoreService.readCertificate(
+                keyStoreService.keystore_path,
+                keyStoreService.keystore_password,
+                id);
+        if (certificate == null)
+            throw new CertificateException("Certificate doesn't exist");
+
+        X509Certificate ca = (X509Certificate) keyStoreService.readCertificate(
+                keyStoreService.keystore_path,
+                keyStoreService.keystore_password,
+                certificate.getIssuerX500Principal().getName()
+        );
+        if (ca == null)
+            throw new CertificateException("Certificate authority doesn't exist");
+
+        IssuerData issuerData = keyStoreService.readIssuerFromStore(
+                keyStoreService.keystore_path,
+                ca.getSubjectX500Principal().getName(),
+                keyStoreService.keystore_password.toCharArray(),
+                ca.getPublicKey().toString().toCharArray()
+        );
+
+        Date now = new Date();
+        X509v2CRLBuilder crlBuilder = new X509v2CRLBuilder(issuerData.getX500name(), now);
+        crlBuilder.addCRLEntry((certificate).getSerialNumber(), now, Extensions.getInstance(reason.getValue()));
+
+        ContentSigner sigGen = new JcaContentSignerBuilder("SHA1withRSA").setProvider("BC").
+                build(issuerData.getPrivateKey());
+        X509CRLHolder crlHolder = crlBuilder.build(sigGen);
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("static/crls/" + ca.getSubjectX500Principal().getName() + ".crl");
+            fileOutputStream.write(crlHolder.getEncoded());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 }
--- PKI/src/main/java/tim6/bsep/pki/service/KeyStoreService.java
+++ PKI/src/main/java/tim6/bsep/pki/service/KeyStoreService.java
@@ -1,15 +1,33 @@
 package tim6.bsep.pki.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import tim6.bsep.pki.keystore.KeyStoreReader;
 import tim6.bsep.pki.keystore.KeyStoreWriter;
+import tim6.bsep.pki.model.IssuerData;
+
+import java.security.cert.Certificate;
 
 public class KeyStoreService {
+
+    @Value("${PKI.keystore_path}")
+    public String keystore_path;
+
+    @Value("${PKI.keystore_password}")
+    public String keystore_password;
 
     private KeyStoreReader keyStoreReader;
     private KeyStoreWriter keyStoreWriter;
 
     public void createKeyStore(String path, String filename, char[] password) {
         keyStoreWriter.createKeyStore(path, filename, password);
+    }
+
+    public Certificate readCertificate(String keyStoreFile, String password, String alias) {
+        return keyStoreReader.readCertificate(keyStoreFile, password, alias);
+    }
+
+    public IssuerData readIssuerFromStore(String keyStoreFile, String alias, char[] password, char[] keyPass) {
+        return keyStoreReader.readIssuerFromStore(keyStoreFile, alias, password, keyPass);
     }
 
 }
