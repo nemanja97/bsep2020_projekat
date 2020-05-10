8b516e158976ea2f838f984aa3b8fc7cf7784cb5
Nemanja Janković <nemanja997@gmail.com>
2020-05-10 22:45:25 +0200

    Fixed cert chain, fxed PKI chain, added zip download for private key and cert chain

--- PKI/src/main/java/tim6/bsep/pki/service/CertificateService.java
+++ PKI/src/main/java/tim6/bsep/pki/service/CertificateService.java
@@ -6,6 +6,7 @@ import tim6.bsep.pki.exceptions.*;
 import tim6.bsep.pki.model.RevocationReason;
 
 import java.io.IOException;
+import java.security.PrivateKey;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -15,6 +16,8 @@ public interface CertificateService {
     X509Certificate createCertificate(String issuerAlias, String alias, X500Name subjectName, String template) throws CertificateNotFoundException, IssuerNotCAException, IssuerNotValidException, UnknownTemplateException, AliasAlreadyTakenException;
 
     String writeCertificateToPEM(X509Certificate certificate) throws CertificateEncodingException, IOException;
+
+    String writePrivateKeyToPEM(PrivateKey privateKey) throws CertificateEncodingException, IOException;
 
     public boolean isCertificateValid(String alias);
 
--- PKI/src/main/java/tim6/bsep/pki/web/v1/controller/CertificateController.java
+++ PKI/src/main/java/tim6/bsep/pki/web/v1/controller/CertificateController.java
@@ -2,6 +2,7 @@ package tim6.bsep.pki.web.v1.controller;
 
 import com.google.gson.GsonBuilder;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.util.io.pem.PemObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -11,15 +12,21 @@ import tim6.bsep.pki.model.CertificateInfo;
 import tim6.bsep.pki.model.CertificateInfoWithChildren;
 import tim6.bsep.pki.model.RevocationReason;
 import tim6.bsep.pki.service.CertificateInfoService;
+import tim6.bsep.pki.service.KeyStoreService;
 import tim6.bsep.pki.service.implementation.CertificateServiceImpl;
 import tim6.bsep.pki.web.v1.dto.CertificateDTO;
 import tim6.bsep.pki.web.v1.dto.CreateCertificateDTO;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping(value = "api/v1/certificates")
@@ -31,6 +38,9 @@ public class CertificateController {
 
     @Autowired
     CertificateInfoService certificateInfoService;
+
+    @Autowired
+    KeyStoreService keyStoreService;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity getCertificates(
@@ -52,15 +62,42 @@ public class CertificateController {
             @PathVariable String alias,
             @RequestParam(value = "format", required = false, defaultValue = "text") String format) throws CertificateEncodingException, IOException, CertificateParsingException {
         X509Certificate certificate = certificateServiceImpl.findByAlias(alias);
+        if (certificate == null)
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
 
         switch (format) {
             case "pem":
-                String pemCertificate = certificateServiceImpl.writeCertificateToPEM(certificate);
+                Certificate[] chain = keyStoreService.readCertificateChain(alias);
+                PrivateKey privateKey = keyStoreService.readPrivateKey(alias);
+                String privateKeyContents = certificateServiceImpl.writePrivateKeyToPEM(privateKey);
 
-                byte[] contents = pemCertificate.getBytes();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                try (ZipOutputStream zipOutputStream = new ZipOutputStream((outputStream))){
+
+                    StringBuilder chainBuilder = new StringBuilder();
+                    for (Certificate c : chain) {
+                        String pemCertificate = certificateServiceImpl.writeCertificateToPEM((X509Certificate) c);
+                        chainBuilder.append(pemCertificate);
+                    }
+
+                    ZipEntry pemEntry = new ZipEntry("certificate.crt");
+                    ZipEntry keyEntry = new ZipEntry("certificate.key");
+
+                    zipOutputStream.putNextEntry(pemEntry);
+                    zipOutputStream.write(chainBuilder.toString().getBytes());
+                    zipOutputStream.closeEntry();
+
+                    zipOutputStream.putNextEntry(keyEntry);
+                    zipOutputStream.write(privateKeyContents.getBytes());
+                    zipOutputStream.closeEntry();
+                } catch (IOException ex) {
+                    return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+
+                byte[] contents = outputStream.toByteArray();
                 HttpHeaders headers = new HttpHeaders();
                 headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-                String filename = "certificate.pem";
+                String filename = "certificate.zip";
                 ContentDisposition contentDisposition = ContentDisposition
                         .builder("inline")
                         .filename(filename)
@@ -88,11 +125,6 @@ public class CertificateController {
         X500Name subjectData = CertificateInfoMapper.nameFromDTO(certificateDTO);
         certificateServiceImpl.createCertificate(certificateDTO.getIssuerAlias(), certificateDTO.getAlias(), subjectData, certificateDTO.getTemplate());
         return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @RequestMapping(name = "/validate", method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity validateCertificate() {
-        return null;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
